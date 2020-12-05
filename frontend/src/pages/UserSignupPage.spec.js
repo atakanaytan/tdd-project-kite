adb9df17c0a4ab6f859fc0c5cf48617269efe672
atakanaytan <aytan.atakan2@gmail.com>
2020-12-06 01:39:34 +0300

    handling button click test cases to the signup page have been implemented.

--- frontend/src/pages/UserSignupPage.spec.js
+++ frontend/src/pages/UserSignupPage.spec.js
@@ -57,6 +57,31 @@ describe('UserSignupPage', () => {
           }
         };
 
+        let button, displayNameInput, usernameInput, passwordInput, passwordRepeat;
+
+        const setupForSubmit = (props) => {
+            
+            const rendered = render(
+                <UserSignupPage {...props} />
+            );
+            
+            const { container, queryByPlaceholderText } = rendered;
+
+            displayNameInput = queryByPlaceholderText('Your display name');
+            usernameInput = queryByPlaceholderText('Your username');
+            passwordInput = queryByPlaceholderText('Your password');
+            passwordRepeat = queryByPlaceholderText('Repeat your password');    
+
+            fireEvent.change(displayNameInput, changeEvent('my-display-name'));
+            fireEvent.change(usernameInput, changeEvent('my-user-name'));
+            fireEvent.change(passwordInput, changeEvent('P4ssword'));
+            fireEvent.change(passwordRepeat, changeEvent('P4ssword'));
+                
+            button = container.querySelector('button');
+            
+            return rendered;
+        }
+
         it('sets the displayName value into state', () => {
             const { queryByPlaceholderText } = render(<UserSignupPage />);
             const displayNameInput = queryByPlaceholderText('Your display name');
@@ -88,6 +113,36 @@ describe('UserSignupPage', () => {
             fireEvent.change(passwordRepeatInput, changeEvent('P4ssword'));
 
             expect(passwordRepeatInput).toHaveValue('P4ssword');
+        });
+        it('calls postSignup when the fields are valid and the actions are provided in props', () => {
+            const actions = {
+                postSignup: jest.fn().mockResolvedValueOnce({})
+            };
+
+            setupForSubmit({ actions });
+            fireEvent.click(button);
+            
+            expect(actions.postSignup).toHaveBeenCalledTimes(1);
+        });
+        it('does not throw exception when clicking the button when actions not provided in props', () => {
+            setupForSubmit();
+            expect(() => fireEvent.click(button)).not.toThrow();
+        });
+        it('calls post with user body the fields are valid', () => {
+            const actions = {
+                postSignup: jest.fn().mockResolvedValueOnce({})
+            };
+            
+            setupForSubmit({ actions });
+            fireEvent.click(button);
+            
+            const expectedUserObject = {
+                username: 'my-user-name',
+                displayName: 'my-display-name',
+                password: 'P4ssword',
+            }
+            
+            expect(actions.postSignup).toHaveBeenCalledWith(expectedUserObject);
         });
     });
  });
