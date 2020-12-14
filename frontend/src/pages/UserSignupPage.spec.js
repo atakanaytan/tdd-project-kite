9b4308e442adf71bb4ef81bfa441339983df0bd9
atakanaytan <aytan.atakan2@gmail.com>
2020-12-14 22:21:21 +0300

    displaying validation error tests have been added for the frontend.

--- frontend/src/pages/UserSignupPage.spec.js
+++ frontend/src/pages/UserSignupPage.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup, fireEvent, waitForDomChange } from '@testing-library/react';
+import { render, cleanup, fireEvent, waitForDomChange, waitForElement } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { UserSignupPage } from './UserSignupPage';
 
@@ -213,5 +213,24 @@ describe('UserSignupPage', () => {
             const spinner = queryByText('Loading...');
             expect(spinner).not.toBeInTheDocument(); 
         });
+        it('displays validation error for displayName when error is received for the field', async () => {
+          const actions = {
+            postSignup: jest.fn().mockRejectedValue({
+              response: {
+                data: {
+                  validationErrors: {
+                    displayName: 'Cannot be null',
+
+                  }
+                }
+              }  
+            })
+          }
+          const { queryByText } = setupForSubmit({ actions });
+          fireEvent.click(button);
+
+          const errorMessage = await waitForElement(() => queryByText('Cannot be null'));
+          expect(errorMessage).toBeInTheDocument();
+      });
     });
  });
