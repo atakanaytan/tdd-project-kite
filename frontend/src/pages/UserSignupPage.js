0a1272af4f5a5dacc8ca1deea658223d3dc636eb
atakanaytan <aytan.atakan2@gmail.com>
2020-12-15 23:40:12 +0300

    dynamic client-side validation error tests have been added.

--- frontend/src/pages/UserSignupPage.js
+++ frontend/src/pages/UserSignupPage.js
@@ -9,27 +9,39 @@ export class UserSignupPage extends React.Component {
         password: '',
         passwordRepeat: '',
         pendingApiCall: false,
-        errors: {}
+        errors: {},
+        passwordRepeatConfirmed: true
     };
 
     onChangeDisplayName = (event) => {
        const value = event.target.value;
-       this.setState({ displayName: value });
+       const errors = {...this.state.errors };
+       delete errors.displayName;
+       this.setState({ displayName: value, errors });
     };
 
     onChangeUserName = (event) => {
         const value = event.target.value;
-        this.setState({ username: value });
+        const errors = {...this.state.errors };
+        delete errors.username;
+        this.setState({ username: value, errors });
     };
 
     onChangePassword = (event) => {
         const value = event.target.value;
-        this.setState({ password: value });
+        const passwordRepeatConfirmed  = this.state.passwordRepeat === value;
+        const errors = {...this.state.errors };
+        delete errors.password;
+        errors.passwordRepeat = passwordRepeatConfirmed ? '' : 'Does not match to password';
+        this.setState({ password: value, passwordRepeatConfirmed, errors });
     };
 
     onChangePasswordRepeat = (event) => {
         const value = event.target.value;
-        this.setState({ passwordRepeat: value });
+        const passwordRepeatConfirmed = this.state.password === value;
+        const errors = {...this.state.errors };
+        errors.passwordRepeat = passwordRepeatConfirmed ? '' : 'Does not match to password';
+        this.setState({ passwordRepeat: value, passwordRepeatConfirmed, errors });
     };
 
     onClickSignup = () => {
@@ -99,14 +111,14 @@ export class UserSignupPage extends React.Component {
                      state={this.state.passwordRepeat}
                      onChange={this.onChangePasswordRepeat}
                      hasError={this.state.errors.passwordRepeat  && true}
-                     error={this.state.errors.passwordRepeat } 
+                     error={this.state.errors.passwordRepeat} 
                    />
                 </div>
                 <div className="text-center">
                     <button 
                       className="btn btn-primary" 
                       onClick={this.onClickSignup}
-                      disabled={this.state.pendingApiCall}
+                      disabled={this.state.pendingApiCall || !this.state.passwordRepeatConfirmed}
                     >
                       {this.state.pendingApiCall && (
                         <div className="spinner-border text-light spinner-border-sm mr-1">
