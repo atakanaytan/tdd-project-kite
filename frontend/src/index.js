df9b72f18e4bba2b8673a88edf17f1d0c240a493
atakanaytan <aytan.atakan2@gmail.com>
2020-12-21 10:03:51 +0300

    login page has been added with placeholder tests.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
-import { UserSignupPage } from './pages/UserSignupPage';
+import { LoginPage } from './pages/LoginPage';
 import * as apiCalls from './api/apiCalls';
 
 const actions = {
   postSignup: apiCalls.signup
 };
 
-ReactDOM.render(
-  <UserSignupPage actions={actions} />,
-  document.getElementById('root')
-);
+ReactDOM.render(<LoginPage />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
