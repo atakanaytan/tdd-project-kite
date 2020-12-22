60374baf84ac82be5f9d43bd561f4cf261983e10
atakanaytan <aytan.atakan2@gmail.com>
2020-12-22 23:15:02 +0300

    new pages have been added with their tests.

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class HomePage extends React.Component{
+    render() {
+        return(
+            <div data-testid="homepage"></div>
+        )
+    }
+}
+
+export default HomePage;
