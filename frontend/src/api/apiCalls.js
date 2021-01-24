0fa7904904ad93923a8848f2fa0fe42e0d733155
atakanaytan <aytan.atakan2@gmail.com>
2021-01-24 22:54:38 +0300

    unable to fetching users on homepage problem has been fixed.

--- frontend/src/api/apiCalls.js
+++ frontend/src/api/apiCalls.js
@@ -12,17 +12,15 @@ export const login = (user) => {
 export const setAuthorizationHeader = ({username, password, isLoggedIn}) => {
   if (isLoggedIn) {
     axios.defaults.headers.common['Authorization'] = `Basic ${btoa( 
-      username + ':'   + password
+      username + ':' + password
     )}`;
   } else {
     delete axios.defaults.headers.common['Authorization'];
   }
 };
 
-export const listUsers = (param = {page: 0, size: 3}) => {
-  const pageHasValueOrDefault = (param.page || 0);
-  const sizeHasValueOrDefault = (param.size || 3);
-  const path = `/api/1.0/users?page=${pageHasValueOrDefault}&size=${sizeHasValueOrDefault}`;
+export const listUsers = (param = {page: 0, size: 3 }) => {
+  const path = `/api/1.0/users?page=${param.page || 0 }&size=${param.size || 3 }`;
   return axios.get(path);
 };
 
