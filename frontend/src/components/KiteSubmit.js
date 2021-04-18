ec832ac12f1897532f1f531aae4d617eb6c7c226
atakanaytan <aytan.atakan2@gmail.com>
2021-04-18 21:34:11 +0300

    sending request to the backend for posting kite

--- frontend/src/components/KiteSubmit.js
+++ frontend/src/components/KiteSubmit.js
@@ -1,8 +1,44 @@
 import React, { Component } from 'react';
 import ProfileImageWithDefault from './ProfileImageWithDefault';
 import { connect } from 'react-redux';
+import * as apiCalls from '../api/apiCalls';
 
 class KiteSubmit extends Component {
+    state = {
+        focused: false,
+        content: undefined 
+    };
+
+    onChangeContent = (event) => {
+        const value = event.target.value;
+        this.setState({ content: value })
+    }
+
+    onClickKite = () => {
+        const body = {
+            content: this.state.content
+        }
+        apiCalls.postKite(body).then((response) => {
+            this.setState({
+                focused: false,
+                content: ''
+            });
+        });
+    }
+
+    onFocus = () => {
+        this.setState({
+            focused: true
+        })
+    }
+    
+    onClickCancel = () => {
+        this.setState({
+            focused: false,
+            content: ''
+        })
+    }
+
     render() {
         return (
             <div className="card d-flex flex-row p-1">
@@ -13,7 +49,23 @@ class KiteSubmit extends Component {
                   image={this.props.loggedInUser.image}
               />  
               <div className="flex-fill">
-                <textarea className="form-control w-100" rows={1} />  
+                <textarea className="form-control w-100" 
+                  rows={this.state.focused ? 3 : 1} 
+                  onFocus={this.onFocus}
+                  value={this.state.content}
+                  onChange={this.onChangeContent}    
+                />
+                {this.state.focused && <div className="text-right mt-1">
+                  <button 
+                        className="btn btn-success" 
+                        onClick={this.onClickKite}
+                        >Kite</button>  
+                  <button 
+                        className="btn btn-light ml-1" 
+                        onClick={this.onClickCancel}
+                    >Cancel
+                  </button>
+                </div>}
               </div>
             </div>
         )
--- frontend/src/components/KiteSubmit.spec.js
+++ frontend/src/components/KiteSubmit.spec.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, waitForDomChange } from '@testing-library/react';
 import KiteSubmit from './KiteSubmit';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import authReducer from '../redux/authReducer';
+import * as apiCalls from '../api/apiCalls';
 
 const defaultState = {
     id: 1,
@@ -48,4 +49,99 @@ describe('KiteSubmit', () => {
             expect(image.src).toContain('/images/profile/' + defaultState.image); 
         });
     }); 
+    describe('Interactions', () => {
+        it('displays 3 rows when focused to textarea', () => {
+            const { container } = setup();
+            const textArea = container.querySelector('textarea');
+            fireEvent.focus(textArea);
+            expect(textArea.rows).toBe(3);
+        });
+        it('displays kite button when focused to textarea', () => {
+            const { container, queryByText } = setup();
+            const textArea = container.querySelector('textarea');
+            fireEvent.focus(textArea);
+            const kiteButton = queryByText('Kite');
+            expect(kiteButton).toBeInTheDocument();
+        });
+        it('displays cancel button when focused to textarea', () => {
+            const { container, queryByText } = setup();
+            const textArea = container.querySelector('textarea');
+            fireEvent.focus(textArea);
+            const cancelButton = queryByText('Cancel');
+            expect(cancelButton).toBeInTheDocument();
+        });
+        it('does not Cancel button when focused to textarea', () => {
+            const { queryByText } = setup();
+            const kiteButton = queryByText('Kite');
+            expect(kiteButton).not.toBeInTheDocument();
+        });
+        it('does not display Cancel button when not focused to textarea', () => {
+            const { queryByText } = setup();
+            const cancelButton = queryByText('Cancel');
+            expect(cancelButton).not.toBeInTheDocument();
+        });
+        it('returns back to unfocused state after clicking the cancel', () => {
+            const { container, queryByText } = setup();
+            const textArea = container.querySelector('textarea');
+            fireEvent.focus(textArea);
+            const cancelButton = queryByText('Cancel');
+            fireEvent.click(cancelButton);
+            expect(queryByText('Cancel')).not.toBeInTheDocument();
+        });
+        it('calls postKite with kite request object when clicking Kite', () => {
+            const { container, queryByText } = setup();
+            const textArea = container.querySelector('textarea');
+            fireEvent.focus(textArea);
+            fireEvent.change(textArea, { target: { value: 'Test kite content' } });
+
+            const kiteButton = queryByText('Kite');
+
+            apiCalls.postKite = jest.fn().mockResolvedValue( {} );
+            fireEvent.click(kiteButton);
+
+            expect(apiCalls.postKite).toHaveBeenCalledWith({
+                content: 'Test kite content'
+            });
+        });
+        it('returns back to unfocused state after successful postKite action', async() => {
+            const { container, queryByText } = setup();
+            const textArea = container.querySelector('textarea');
+            fireEvent.focus(textArea);
+            fireEvent.change(textArea, { target: { value: 'Test kite content' } });
+
+            const kiteButton = queryByText('Kite');
+
+            apiCalls.postKite = jest.fn().mockResolvedValue( {} );
+            fireEvent.click(kiteButton);
+
+            await waitForDomChange();
+
+            expect(queryByText('Kite')).not.toBeInTheDocument();
+        });
+        it('clear content after successful postKite action', async() => {
+            const { container, queryByText } = setup();
+            const textArea = container.querySelector('textarea');
+            fireEvent.focus(textArea);
+            fireEvent.change(textArea, { target: { value: 'Test kite content' } });
+
+            const kiteButton = queryByText('Kite');
+
+            apiCalls.postKite = jest.fn().mockResolvedValue( {} );
+            fireEvent.click(kiteButton);
+
+            await waitForDomChange();
+
+            expect(queryByText('Test kite content')).not.toBeInTheDocument();
+        });
+        it('clear content after clicking cancel', () => {
+            const { container, queryByText } = setup();
+            const textArea = container.querySelector('textarea');
+            fireEvent.focus(textArea);
+            fireEvent.change(textArea, { target: { value: 'Test kite content' } });
+            
+            fireEvent.click(queryByText('Cancel'));
+            
+            expect(queryByText('Test kite content')).not.toBeInTheDocument();
+        });
+    });
 });
